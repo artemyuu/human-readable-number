1ce994cce373eb2c00501ebd55e31768dcd0d576
artemyuu <60568831+artemyuu@users.noreply.github.com>
2020-03-19 17:36:59 +0400

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
+const TENS = [
+    'ten', 'twenty', 'thirty', 'forty', 'fifty',
+    'sixty', 'seventy', 'eighty', 'ninety'
+  ];
+const ONE__NINETEEN = [
+    'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 
+    'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 
+    'seventeen', 'eighteen', 'nineteen'
+];
+
 module.exports = function toReadable (number) {
+    let hundreds, ones, tens, res = [];
+    if(number < 20){
+        return ONE__NINETEEN[number];
+    }
   
-}
+    if(number < 100){
+        ones = number % 10;
+        tens = Math.floor(number / 10);
+  
+        res.push(TENS[tens-1]);
+        res.push(toReadable(ones));
+
+        return res.filter(function(value){
+              return value != "zero";
+        }).join(" ");
+    }
+  
+    hundreds = Math.floor(number / 100);
+    res.push(toReadable(hundreds));
+    res.push("hundred");
+    res.push(toReadable(number % 100));
+  
+    return res.filter(function(value){
+      return  value != "zero";
+    }).join(" ");
+  }
